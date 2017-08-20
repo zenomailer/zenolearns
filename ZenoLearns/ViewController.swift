3f7bfeb2440304968ae2bab6b8956602d2e92a9e
Sid K <SidK@Sids-Mac-mini.local>
2017-08-20 13:02:46 +0530

    Hello World

--- ZenoLearns/ViewController.swift
+++ ZenoLearns/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello World")
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +25,4 @@ class ViewController: UIViewController {
 
 }
 
+	
